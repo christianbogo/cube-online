5c9dfb9a9eb73cf3fa0414cf8286f0e873fe2402
coach-christian-tools <coachchristian@velocity-swimming.com>
2026-09-02 22:55:18 -0700

    chore: remove obsolete manual patch scripts after application

--- patch_layout3.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/components/layout/Layout.tsx', 'utf8');
-
-code = code.replace(
-    /height: isMobile \? '280px' : '100%'/g,
-    "height: isMobile ? 'auto' : '100%'"
-);
-
-fs.writeFileSync('src/components/layout/Layout.tsx', code);
